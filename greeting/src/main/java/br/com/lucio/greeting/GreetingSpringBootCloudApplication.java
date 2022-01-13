4e08695b0b38f757dc859cc7ba39c89bfe351f9d
Rodrigo Lúcio <luciodigo@gmail.com>
2022-01-13 15:56:59 -0300

    Criado projeto config server

--- greeting/src/main/java/br/com/lucio/greeting/GreetingSpringBootCloudApplication.java
+++ greeting/src/main/java/br/com/lucio/greeting/GreetingSpringBootCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoMicroservicesSpringBootCloudApplication {
+public class GreetingSpringBootCloudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CursoMicroservicesSpringBootCloudApplication.class, args);
+        SpringApplication.run(GreetingSpringBootCloudApplication.class, args);
     }
 
 }
