1dd5131e64d4280f8ea62ad1071998d9d47c1b92
Rodrigo Lúcio <luciodigo@gmail.com>
2022-01-27 17:36:55 -0300

    Mostrando o log das requisições

---
+++ api-gateway/src/main/java/br/com/lucio/filter/LoggingFilter.java
@@ -0,0 +1,21 @@
+package br.com.lucio.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter{
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		log.info("Original request path -> {}", exchange.getRequest().getPath());
+		return chain.filter(exchange);
+	}   
+
+}
