75dcfe2801bc5983552ce71775d8ac282f57b680
Rodrigo Lúcio <luciodigo@gmail.com>
2022-02-10 23:00:43 -0300

    Testando o Circuit Breaker

--- book/src/main/java/br/com/lucio/controller/FooBarController.java
+++ book/src/main/java/br/com/lucio/controller/FooBarController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,8 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FooBarController {
 
+	// @CircuitBreaker =funciona igual o Retry, porem é inteligente, nao fica entrando no metodo sempre. A cada pouco ele entrará no metodo para ver se ainda esta com erro ou nao, 
+	// e dependendo disso, joga para o fallback ou nao
+	// Se mandar várias requisiçoes de uma só vez, e estiver com erro, ele nao vai entrar no metodo, logo a mensagem do log.info nao sera exibida.
+	// Estilo um disjuntor de casa.
+	
 	@GetMapping
-	@Retry(name = "foo-bar", fallbackMethod = "methodWithError") //pode configurar no aplication.yml
+	//@Retry(name = "foo-bar", fallbackMethod = "methodWithError") //pode configurar no aplication.yml
+	@CircuitBreaker(name = "foo-bar", fallbackMethod = "methodWithError") 
 	public String fooBar() {
 		log.info("Request to foo-bar is received.");
 		var response = new RestTemplate().getForEntity("http://localhost:1234/foo-bar", String.class);
